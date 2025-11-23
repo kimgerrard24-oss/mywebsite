2cf4e9c36e00a69c8608092a13d5740c09b152c3
kimgerrard24 <kimgerrard24@gmail.com>
2025-11-23 09:12:27 +0700

    Add FB domain verification

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,16 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="facebook-domain-verification" content="so0pf4hgs1cit7mpuxefhrc6ndfs9c" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
