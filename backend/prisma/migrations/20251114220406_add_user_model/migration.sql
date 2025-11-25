2c1d134dc6245af0e82f30e419812a83d0732b0c
kimgerrard24 <kimgerrard24@gmail.com>
2025-11-25 15:02:31 +0700

    Add backend prisma migrations (baseline)

---
+++ backend/prisma/migrations/20251114220406_add_user_model/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[firebaseUid]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `provider` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `providerId` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "password",
+ADD COLUMN     "firebaseUid" TEXT,
+ADD COLUMN     "name" TEXT,
+ADD COLUMN     "provider" TEXT NOT NULL,
+ADD COLUMN     "providerId" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firebaseUid_key" ON "User"("firebaseUid");
