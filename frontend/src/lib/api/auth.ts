875bf316abca1b407468adab1af07815be97491b
kimgerrard <kimgerrard24@gmail.com>
2025-12-03 23:47:57 +0700

    modified

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -4,15 +4,18 @@ import axios from 'axios';
 
 const API = process.env.NEXT_PUBLIC_BACKEND_URL;
 
+if (!API) {
+  throw new Error('NEXT_PUBLIC_BACKEND_URL is missing. Please check frontend/.env.production');
+}
+
 export async function registerUser(body: {
   email: string;
   username: string;
   password: string;
 }) {
   const res = await axios.post(`${API}/auth/local/register`, body, {
-    withCredentials: true,   // สำคัญมาก
+    withCredentials: true,
   });
 
   return res.data;
 }
-
