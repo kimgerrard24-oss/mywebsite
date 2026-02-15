d653d3dc3b2694c56e37b6e08a06276876885b20
kimgerrard <kimgerrard24@gmail.com>
2026-02-15 11:28:29 +0700

    avatarcoverchecked1

--- frontend/src/components/profile/CoverUpdateModal.tsx
+++ frontend/src/components/profile/CoverUpdateModal.tsx
@@ -4,17 +4,54 @@
 
 import CoverUpdateComposer from "./CoverUpdateComposer";
 import { useCoverUpdateStore } from "@/stores/cover-update.store";
+import { useCurrentProfileMedia } from "@/hooks/useCurrentProfileMedia";
 
-export default function CoverUpdateModal() {
+type Props = {
+  currentMedia: ReturnType<typeof useCurrentProfileMedia>;
+};
+
+export default function CoverUpdateModal({
+  currentMedia,
+}: Props) {
+
   const { draft, clear } = useCoverUpdateStore();
 
   if (!draft) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50">
-      <div className="bg-white rounded-xl w-full max-w-lg p-6">
-        <CoverUpdateComposer onClose={clear} />
+    <div
+      className="
+        fixed
+        inset-0
+        bg-black/40
+        flex
+        justify-center
+        items-center
+        z-50
+        px-4
+      "
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        className="
+          bg-white
+          rounded-xl
+          w-full
+          max-w-lg
+          p-5
+          sm:p-6
+          shadow-xl
+        "
+      >
+
+        <CoverUpdateComposer
+          currentMedia={currentMedia}
+          onClose={clear}
+        />
+
       </div>
     </div>
   );
 }
+
